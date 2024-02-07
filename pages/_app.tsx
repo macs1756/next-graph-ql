f5f657378a083ef510d7b2fd757547512cc02209
macs1756 <kindrativmaksum312@gmail.com>
2024-02-07 20:46:30 +0200

    v1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { ApolloProvider } from '@apollo/client';
+import { AppProps } from 'next/app';
+import client from '../apolloClient'; 
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+export default App;
